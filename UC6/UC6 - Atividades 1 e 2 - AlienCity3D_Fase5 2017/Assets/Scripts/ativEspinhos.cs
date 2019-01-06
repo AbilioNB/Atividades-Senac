aec3b4f42a190832814d96a6103ad9e9c007a50a
Abílio Nogueira <abilionbarros@gmail.com>
2019-01-06 14:42:50 -0200

    ativação dos espinhos

---
+++ UC6/UC6 - Atividades 1 e 2 - AlienCity3D_Fase5 2017/Assets/Scripts/ativEspinhos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ativEspinhos : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject EspinhosGo;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			EspinhosGo.gameObject.GetComponent<Rigidbody>().useGravity=true;
+		}
+	}
+}
