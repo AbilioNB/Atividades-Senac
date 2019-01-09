ea1b3d6c6d7c578f2ddfeeea4a76aefa2010b559
AbilioN <abilionbarros@gmail.com>
2019-01-09 00:17:11 -0300

    Atualizações no projeto
    *Incluida Animação da porta
    *todos os colider aplicados

---
+++ UC6/UC6 - Atividades 1 e 2 - AlienCity3D_Fase5 2017/Assets/Scripts/Scripts Collisores/abrePortas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abrePortas : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject door;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+				door.gameObject.GetComponent<Animation>().Play ();
+				print("Abrir portas");
+		}
+	}
+}
---
+++ UC6/UC6 - Atividades 1 e 2 - AlienCity3D_Fase5 2017/Assets/Scripts/Scripts Collisores/movPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject buffer;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+				buffer.gameObject.GetComponent<Animation>().Play ();
+				print("Ativei as plataforma");
+		}
+	}
+}
